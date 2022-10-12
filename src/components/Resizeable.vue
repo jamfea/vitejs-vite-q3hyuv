6562dae87144b8fe80e83bbb3b0458fee97313fa
jamfea <113727699+jamfea@users.noreply.github.com>
2022-10-12 12:07:59 -0400

    Initial commit

---
+++ src/components/Resizeable.vue
@@ -0,0 +1,14 @@
+<script setup>
+import { ref } from 'vue';
+const values = ref([]);
+function add() {
+  values.value.push('hello!');
+}
+</script>
+
+<template>
+  <div @click="add">Add</div>
+  <div v-for="v in values">
+    {{ v }}
+  </div>
+</template>
